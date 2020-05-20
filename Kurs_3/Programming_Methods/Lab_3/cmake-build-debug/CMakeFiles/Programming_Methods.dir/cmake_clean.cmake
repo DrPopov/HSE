0057985b9ff1845626a3625c0622cc7e27e9e13c
DrPopov <yulpopov67@gmail.com>
2020-05-20 19:59:38 +0300

    Add linear and binary search

---
+++ Kurs_3/Programming_Methods/Lab_3/cmake-build-debug/CMakeFiles/Programming_Methods.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programming_Methods.dir/main.cpp.o"
+  "Programming_Methods"
+  "Programming_Methods.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Programming_Methods.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
